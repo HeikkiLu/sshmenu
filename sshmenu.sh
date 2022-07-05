8c5d8b1c47a0e9f7d9a623ae19d1e7c1be83ed07
HeikkiLu <heikki.luukkonen@gmail.com>
2022-07-05 14:42:53 +0300

    connect to tmux and create ssh

--- sshmenu.sh
+++ sshmenu.sh
@@ -4,7 +4,7 @@
 
 input="$HOME/.ssh/config"
 remote_hosts=()
-headers="# [a-zA-Z]+"
+config_found=true
 
 if [[ -e $input ]]; then
   while read -r line;
@@ -16,18 +16,24 @@ if [[ -e $input ]]; then
     fi
   done < "$input"
 else
-  echo "Config does not exist in $HOME/.ssh/, please create one first!"
+  echo "No config found in $HOME/.ssh/!";
+  config_found=false;
 fi
 
 remote_hosts+=("Quit")
 
-PS3='Select connection '
-select host in "${remote_hosts[@]}"
-do
-  if [ "$host" = "Quit" ]
-    then break
-  fi
-  if [ "$host" ]
-    then $(ssh $host);  
-  fi
-done 
+if [ "$config_found" = true ]; then
+  PS3='Select connection '
+  select host in "${remote_hosts[@]}"
+  do
+    if [ "$host" = "Quit" ]
+      then break
+    else
+      tmux new-session -d -s "$host";
+      tmux send-keys -t "$host" "ssh $host" Enter;
+      tmux attach -t "$host"
+      break; 
+    fi
+  done  
+fi
+
